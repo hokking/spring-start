ee3c34eb22208f4d852d4f47b9e8707452be9234
hokking <qkrwldms1403@naver.com>
2024-05-16 14:46:24 +0900

    commit

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
-    @Override
     Optional<Member> findByName(String name);
 }
